63163329707b513e0c1ecd848ddb736f3f262f6c
Zebado <sbasualdo.antares@gmail.com>
2025-04-11 14:56:56 -0300

    Update

--- Visceral Limbo/Assets/ScriptsS/M_Options.cs
+++ Visceral Limbo/Assets/ScriptsS/M_Options.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class M_Options : MonoBehaviour
 {
+    [SerializeField] AudioMixer audioMixer;
     public void FullScreenOption(bool FullScreen)
     {
         Screen.fullScreen = FullScreen;
+    }
+    public void ChangeVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+    }
+    public void Quality(int index)
+    {
+        QualitySettings.SetQualityLevel(index);
     }
 }
