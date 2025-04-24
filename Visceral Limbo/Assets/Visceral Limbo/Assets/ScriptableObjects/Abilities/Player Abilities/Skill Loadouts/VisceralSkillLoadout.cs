0c6bcc4e037ab5937a045647e9683aea587bcf93
patriciomalvasiomaddalena <130863754+patriciomalvasiomaddalena@users.noreply.github.com>
2025-04-24 01:24:06 -0300

    añadido de skill LinQ mas comentarios en SkillManager / SkillLogic

--- Visceral Limbo/Assets/Visceral Limbo/Assets/ScriptableObjects/Abilities/Player Abilities/Skill Loadouts/VisceralSkillLoadout.cs
+++ Visceral Limbo/Assets/Visceral Limbo/Assets/ScriptableObjects/Abilities/Player Abilities/Skill Loadouts/VisceralSkillLoadout.cs
@@ -6,5 +6,8 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Visceral_Limbo/Combat Data/SkillLoadout/SkillLoadoutSO")]
 public class VisceralSkillLoadout : ScriptableObject
 {
+    /// <summary>
+    /// el loadout del skillmanager
+    /// </summary>
     public List<Visceral_AbilitySO> Skill_Loadout = new();
 }
--- Visceral Limbo/Assets/Visceral Limbo/scripts/Base_Classes/Skils Systems/Visceral_SkillManager.cs
+++ Visceral Limbo/Assets/Visceral Limbo/scripts/Base_Classes/Skils Systems/Visceral_SkillManager.cs
@@ -7,8 +7,17 @@ using UnityEngine;
 
 public class Visceral_SkillManager : Visceral_Script
 {
-    private PlayerContext UserContext;
+    /// <summary>
+    /// contexto del usuario
+    /// </summary>
+    private PlayerContext UserContext; 
+    /// <summary>
+    /// diccionario de habilidades y sus IDs
+    /// </summary>
     private Dictionary<string, Visceral_SkillLogic> AbilityDicc = new();
+    /// <summary>
+    /// diccionario de IDs y Cooldowns de habilidades
+    /// </summary>
     private Dictionary<string,float> CooldownDicc= new();
 
     [SerializeField] private VisceralSkillLoadout Loadout;
@@ -17,23 +26,33 @@ public class Visceral_SkillManager : Visceral_Script
     {
         UserContext = GetComponent<PlayerContext>();
 
+        // por cada scriptableObject del Loadout, creamos una instancia en escena de la habilidad
         foreach(var Data in Loadout.Skill_Loadout)
         {
+            //instanciado de objeto
             var SkillGO = Instantiate(Data.behaviorComponentPrefab, UserContext.PlayerTransform);
             SkillGO.transform.SetParent(UserContext.PlayerTransform);
+            //iniciado de skill
             var Behaviour = SkillGO.GetComponent<Visceral_SkillLogic>();
             Behaviour.Initialize(Data,this,UserContext);
+            //guardado de skill
             AbilityDicc[Data.SkillID] = Behaviour;
             CooldownDicc[Data.SkillID] = 0f;
+            print(Data.SkillID);
         }
     }
 
+    /// <summary>
+    /// update de la logica interna del skillmanager
+    /// </summary>
     public override void VS_RunLogic()
     {
         UpdateCooldowns();
     }
 
-
+    /// <summary>
+    /// update de cooldowns
+    /// </summary>
     private void UpdateCooldowns()
     {
         var keys = new List<string>(CooldownDicc.Keys); 
@@ -47,7 +66,11 @@ public class Visceral_SkillManager : Visceral_Script
         }
     }
 
-
+    /// <summary>
+    /// Intentar de usar skill guardada en el SkillManager
+    /// </summary>
+    /// <param name="SkillName">ID de skill guardada</param>
+    /// <returns></returns>
     public bool TryUseSkill(string SkillName)
     {
         if(!AbilityDicc.TryGetValue(SkillName, out var ability))
@@ -57,7 +80,7 @@ public class Visceral_SkillManager : Visceral_Script
         }
         if (CooldownDicc[SkillName] > 0f)
         {
-            Debug.Log("<Color=lightblue> Visceral Warning: Skill " + SkillName + " on cooldown </Color>");
+            Debug.Log("<Color=lightblue> Visceral Warning: Skill " + SkillName + " on cooldown " + CooldownDicc[SkillName] + "</Color>");
             return false;
         }
 
@@ -68,6 +91,11 @@ public class Visceral_SkillManager : Visceral_Script
 
     }
 
+    /// <summary>
+    /// Booleano para ver si la habilidad esta en cooldown
+    /// </summary>
+    /// <param name="SkillID"> ID de la Skill deseada</param>
+    /// <returns></returns>
     public bool IsOnCooldown(string SkillID)
     {
         return Time.time < CooldownDicc[SkillID];
